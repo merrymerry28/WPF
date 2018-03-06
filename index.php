d05e6422bc22fc82f91513060ef4246d7b0f9079
merrymerry28 <30824631+merrymerry28@users.noreply.github.com>
2018-03-06 20:42:02 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,199 @@
+<html>
+	<head>
+		<title>Home</title>
+		<style>
+		body {font-family: "Lato", sans-serif;
+				background-image: url("4627233587_1024x661.jpg");
+								height: 100%; 
+								background-position: center;
+								background-repeat: no-repeat;
+								background-size: cover;}
+			/* Style the tab */
+					div.tab {
+						overflow: hidden;
+						border: 1px solid #ccc;
+						background-color: Dodgerblue;
+					}
+
+					/* Style the buttons inside the tab */
+					div.tab button {
+						background-color: inherit;
+						float: left;
+						border: none;
+						outline: none;
+						cursor: pointer;
+						padding: 14px 16px;
+						transition: 0.3s;
+						font-size: 17px;
+					}
+					/* Change background color of buttons on hover */
+					div.tab button:hover {
+						background-color: #ddd;
+					}
+
+					/* Create an active/current tablink class */
+					div.tab button.active {
+						background-color: #ccc;
+					}
+
+					/* Style the tab content */
+					.tabcontent {
+						display: none;
+						padding: 6px 12px;
+						border: 1px solid #ccc;
+						border-top: none;
+					}
+					
+					div.gallery 		{
+								margin: 5px;
+								border: 1px solid #ccc;
+								float: left;
+								width: 180px;
+							}
+
+					div.gallery:hover {
+								border: 1px solid #777;
+								background-color: white;
+							}
+
+					div.gallery img	{
+								width: 100%;
+								height: auto;
+							}
+							
+					div.abg	{	height: 500px;
+								background-color: transparent;
+								border: none;
+								border-radius: 30px;
+								margin-top: 50px;
+								margin-left: 100px;
+								margin-right: 100px;
+								}
+								
+					div.desc 	{
+								padding: 15px;
+								text-align: center;
+								}
+								
+					.sidenav 	{
+									height: 100%;
+									width: 0;
+									position: fixed;
+									z-index: 1;
+									top: 0;
+									left: 0;
+									background-color: #111;
+									overflow-x: hidden;
+									transition: 0.5s;
+									padding-top: 60px;
+								}
+
+								.sidenav a {
+									padding: 8px 8px 8px 32px;
+									text-decoration: none;
+									font-size: 25px;
+									color: #818181;
+									display: block;
+									transition: 0.3s;
+								}
+
+								.sidenav a:hover, .offcanvas a:focus{
+									color: #f1f1f1;
+								}
+
+								.sidenav .closebtn {
+									position: absolute;
+									top: 0;
+									right: 25px;
+									font-size: 36px;
+									margin-left: 50px;
+								}
+
+								@media screen and (max-height: 450px) {
+								  .sidenav {padding-top: 15px;}
+								  .sidenav a {font-size: 18px;}
+								}
+		</style>
+	</head>
+	
+	<body bgcolor="white">
+	
+	<table>
+	<tr>
+		<td width='204' rowspan='100' bgcolor='#F5F5F5' align="center"><img src='logo.jpg' width='300' height='300' style='float:center;'/></td></tr>
+	<tr>
+		 <center><td width="1300" bgcolor='#F5F5F5'><font face="arial" color ="Black"><a name="atas"><h3><center>MADRASAH ALIYAH NURUL HUDA BADEAN
+BANYUWANGI</center></td></h3></font></a></center>&nbsp;&nbsp;&nbsp;
+	
+	</tr>
+	</table>
+	
+	<table align="center" border="0"  width="1280" cellspacing="2" cellpadding="10">
+		<tr align="center" width="1400">
+			<td colspan="3" >
+			<br>
+			<div class="tab">
+				<button class="tablinks" onclick="openCity(event, 'London')">Beranda</button>
+				<button class="tablinks" onclick="openCity(event, 'Paris')">Profil</button>
+				<button class="tablinks" onclick="openCity(event, 'London')">Informasi</button>
+				<button class="tablinks" onclick="openCity(event, 'Paris')">Contact</button>
+				<button class="tablinks" onclick="openCity(event, 'Tokyo')">Alumni</button>
+				<button class="tablinks" onclick="openCity(event, 'London')">Prestasi</button>
+			 </div> 
+			</td>
+		</tr>
+		<tr  align="left" valign="top" > 
+			<td width="250"  bgcolor="Dodgerblue">
+			
+			<h3><ul Style="list-style-type:square"><font face="modern" color="Skyblue" class="dropdown-categories">News Categgories</h3></font>
+			<font color="Skyblue" face="rod"><a href="Content.html"><li>Kurikulum</a></li>&nbsp;&nbsp;&nbsp;
+			<li><a href="health.html">Kesiswaan</a></li>&nbsp;&nbsp;&nbsp;
+			<li><a href="content.html">Sarpras</a></li>&nbsp;&nbsp;&nbsp;
+			<li><a href="movie.html">Osis</a></li>&nbsp;&nbsp;&nbsp;
+			</ul></font>
+			<br>
+			
+			<h2>GALLERY UPDATES</h2>
+			<marquee direction="alternate" scrollamount="1"><a href="fashion.html#fashion2"><img src="bg2.jpg" height="75" width="75" >
+			<img src="bg4.jpg" height="75" width="75"></a>
+			<a href="movie.html#movie1"><img src="bg6.jpg" height="75" width="75" >
+			<img src="bg3.jpg" height="75" width="75">
+			<a href="movie.html#movie2"><img src="halima-aden.jpg" height="75" width="75" ><img src="movie_4.jpg" height="75" width="120">
+			</a></marquee>
+			</td>
+			
+			<br>
+			<td  bgcolor="Dodgerblue" cellspacing="10">
+            <br/>
+        
+    </body>
+		</tr>
+		
+		
+		<tr align="center" valign="top">
+			<td height="150" colspan="3" bgcolor="yellow">
+			<p><b><strong>Follow Us :</b></strong></p>
+			<br>
+			<center><a href="https://www.facebook.com/https://www.facebook.com/profile.php?id=100013826490014"><img alt="facebook" border="0" 
+			src="fbicon.png" height="48" width="48" /></a>&nbsp;
+			<a href="https://www.twitter.com/chamcham_mrz"><img alt="twitter" border="0" 
+			src="Twitter-icon.png" height="48" width="48" title="Follow me" /></a>&nbsp;
+			<a href="https://www.instagram.com/sichamgz"><img alt="instagram" border="0" 
+			src="instagram-Logo-PNG-Transparent-Background-download.png" height="48" width="48" title="Follow me" /></a>&nbsp;<br><br>
+			&phone; 085737329896 <spasi><br>
+			E-mail : badean@gmail.com
+			</center>
+			<p align="right"><a href="#atas">Back To Top...</a></p>
+			
+			
+			</td>
+		</tr>
+		<tr align="center">
+			<td colspan="3" height="75">
+				<center>Copyright &copy; 2017 By Sri Wulandari I.| All Rights Reserved</center>
+			</td>
+		</tr>
+	</table>
+	
+	</body>
+</html>
